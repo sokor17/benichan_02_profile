3c7355e02fc4139a8c43859edfd47fba982cc44a
YuKono <cosmos17128@gmail.com>
2021-05-12 02:27:51 +0900

    プロフィールアプリの完成

--- benichan_02_profile/ViewController.swift
+++ benichan_02_profile/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //画像表示変数
+    @IBOutlet var profileImageView :UIImageView!
+    //ラベル表示変数
+    @IBOutlet var profileCommentLabel :UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // 画像を見えなくする
+        profileImageView.isHidden = true
+        //ラベルを見えなくする
+        profileCommentLabel.isHidden = true
     }
-
+   
+    @IBAction func tapButton1(){
+        // 画像を見えるようにする
+        profileImageView.isHidden = false
+        //ラベルを見えるようにする
+        profileCommentLabel.isHidden = false
+        
+    }
 
 }
 
